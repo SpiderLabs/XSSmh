105e8354907dbfbcfe687e08a94b00652605220e
unicornFurnace <dcrowley@trustwave.com>
2013-07-19 16:16:54 -0500

    XSSmh is now part of MCIR

--- setup.sh
+++
@@ -1,2 +0,0 @@
-echo "Hey there\! Thanks for installing XSSmh. Enjoy\!"
-chmod 666 pxss.html && echo "Hooray\! Everything went fine\!" || echo "Couldn't chmod pxss.html. Do it manually or run setup.sh as root.";
